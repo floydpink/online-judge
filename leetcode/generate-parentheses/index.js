02c1ce5daa7d5b9e1786893a38acd95587386fda
Floyd Pink <imbleedingme@googlemail.com>
2015-11-15 14:17:14 -0500

    forgot the generate parens logic

---
+++ leetcode/generate-parentheses/index.js
@@ -0,0 +1,29 @@
+var generateParensRecursive = function (leftCount, rightCount, path, results, position) {
+  if (leftCount < 0 || rightCount < leftCount) {
+    return;
+  }
+  if (leftCount === 0 && rightCount === 0) {
+    results.push(path.join(''));
+  } else {
+    if (leftCount > 0) {
+      path[position] = '(';
+      generateParensRecursive(leftCount - 1, rightCount, path, results, position + 1);
+    }
+    if (rightCount > leftCount) {
+      path[position] = ')';
+      generateParensRecursive(leftCount, rightCount - 1, path, results, position + 1);
+    }
+  }
+};
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  var results = [], path = new Array(n * 2);
+  generateParensRecursive(n, n, path, results, 0);
+  return results;
+};
+
+console.log(generateParenthesis(3).join(', '));
