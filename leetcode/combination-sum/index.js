f0e65215c63b3f514fb0ecf4cd24bb6089e34ea8
Floyd Pink <imbleedingme@googlemail.com>
2015-11-14 22:02:27 -0500

    dfs for combinationSum with infinite selections

---
+++ leetcode/combination-sum/index.js
@@ -0,0 +1,34 @@
+var depthFirstSearch = function (candidates, current, remaining, path, results) {
+  // console.log('.>', current, remaining);
+  if (remaining < 0) {
+    return;
+  }
+  if (remaining === 0) {
+    // console.log('>>', path);
+    results.push(path.slice());
+    return;
+  }
+  for (var i = current; i < candidates.length; i++) {
+    var temp = path.slice();
+    temp.push(candidates[i]);
+    if (remaining < candidates[i]) break;
+    depthFirstSearch(candidates, i, remaining - candidates[i], temp, results);
+    // console.log('.<', i, remaining);
+  }
+};
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+  candidates = candidates.filter(function (val, idx, self) { return self.indexOf(val) === idx;}).sort(function (a, b) { return a - b;});
+  var results = [], path = [];
+  depthFirstSearch(candidates, 0, target, path, results);
+  return results;
+};
+
+console.log(combinationSum([2], 1));
+console.log(combinationSum([1], 1));
+console.log(combinationSum([1, 2, 3, 4, 5], 10));
