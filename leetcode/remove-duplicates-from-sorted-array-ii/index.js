98601a5e2831e801ca42757abe4d9db87b3ad0e3
Haridas Pachuveetil <imbleedingme@googlemail.com>
2015-11-17 14:41:30 -0500

    remove duplicates from sorted array - solved but wrong answer

---
+++ leetcode/remove-duplicates-from-sorted-array-ii/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  var firstIndex = 0, result = [];
+  nums.forEach(function (curr, index, arr) {
+    if (arr[index] === arr[index - 1]) {
+      if (index === firstIndex + 1) {
+        result.push(curr);
+      }
+    } else {
+      firstIndex = index;
+      result.push(curr);
+    }
+  });
+  return result;
+};
+
+console.log(removeDuplicates([1, 1]));
+
+//console.log(removeDuplicates([]));
+//console.log(removeDuplicates([1]));
+//console.log(removeDuplicates([1, 1]));
+//console.log(removeDuplicates([1, 2, 3]));
+//console.log(removeDuplicates([1, 1, 2, 2, 3, 5]));
+//console.log(removeDuplicates([1, 1, 1, 2, 2, 2, 3, 5, 5]));
--- leetcode/shortest-palindrome/index.js
+++ leetcode/shortest-palindrome/index.js
@@ -1,18 +1,26 @@
-// Inspired by the elegant Java solution at  https://leetcode.com/discuss/51223/my-7-lines-recursive-java-solution
+'use strict';
+
 /**
  * @param {string} s
  * @return {string}
  */
+var counter = 0;
 var shortestPalindrome = function (s) {
+  console.log('.>', ++counter, s);
   var j = 0;
   for (var i = s.length - 1; i >= 0; i--) {
     if (s.charAt(i) === s.charAt(j)) {
-      j += 1;
+      j++;
     }
   }
   if (j === s.length) {
     return s;
   }
-  var suffix = s.substring(j);
-  return suffix.split('').reverse().join('') + shortestPalindrome(s.substring(0, j)) + suffix;
+  var suffix = s.substring(j), reversed = suffix.split('').reverse().join('');
+  return reversed + shortestPalindrome(s.substring(0, j)) + suffix;
 };
+
+console.log(shortestPalindrome('malayalam'));
+console.log(shortestPalindrome('malayalamp'));
+console.log(shortestPalindrome('malayzazlam'));
+//console.log(shortestPalindrome('malamalayalama'));
