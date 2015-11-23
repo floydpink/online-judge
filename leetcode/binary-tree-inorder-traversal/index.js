9dac0b541d55bc70cb5fdcb11b06bb828f4e0665
Floyd Pink <imbleedingme@googlemail.com>
2015-11-23 17:49:45 -0500

    solved binary tree inorder traversal

---
+++ leetcode/binary-tree-inorder-traversal/index.js
@@ -0,0 +1,40 @@
+var iOT = function (root, results) {
+  if (!root) return;
+  iOT(root.left, results);
+  results.push(root.val);
+  iOT(root.right, results);
+};
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  var results = [];
+  iOT(root, results);
+  return results;
+};
+
+var getTree = function (array) {
+  if (array.length === 0) {
+    return null;
+  }
+  var rootIndex = Math.floor(array.length / 2);
+  var tree = new TreeNode(array[rootIndex]);
+  tree.left = getTree(array.slice(0, rootIndex));
+  tree.right = getTree(array.slice(rootIndex + 1));
+  return tree;
+};
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+console.log(inorderTraversal(getTree([2, 1])));
