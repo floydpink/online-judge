ae4d3cb49f5204ce220cef0f58537842f7b252b7
Floyd Pink <imbleedingme@googlemail.com>
2015-11-23 10:22:19 -0500

    solved valid-palindrome

---
+++ leetcode/valid-palindrome/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  var chars = s.split('').filter(function (c) { return /[a-z0-9]/.test(c.toLowerCase()); });
+  for (var i = 0; i < Math.floor(chars.length / 2); i++) {
+    if (chars[i].toLowerCase() !== chars[chars.length - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('aA'));
+console.log(isPalindrome('1a2'));
+console.log(isPalindrome('1a1'));
