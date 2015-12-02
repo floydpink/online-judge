39c389aff9a2e568de18b9c06b6dc3009058ce43
Haridas Pachuveetil <imbleedingme@googlemail.com>
2015-12-02 18:06:02 -0500

    3sum closest

---
+++ leetcode/3sum-closest/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  nums = nums.sort(function (a, b) {return a - b;});
+  var answer = nums[0] + nums[1] + nums[2];
+  for (var i = 0; i < nums.length - 2; i++) {
+    var low = i + 1, high = nums.length - 1;
+    while (low < high) {
+      var sum = nums[i] + nums[low] + nums[high];
+      if (Math.abs(target - sum) < Math.abs(target - answer)) {
+        answer = sum;
+        if (answer === target) return answer;
+      }
+      if (sum > target) {
+        high--;
+      } else {
+        low++;
+      }
+    }
+  }
+  return answer;
+};
+
+console.log(threeSumClosest([0, 2, 1, -3], 1));
+console.log(threeSumClosest([1, 2, 4, 8, 16, 32, 64, 128], 82));
