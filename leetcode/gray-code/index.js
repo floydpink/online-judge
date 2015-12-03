02d55a914a70e80937bb15182865124a6b32d6de
Floyd Pink <imbleedingme@googlemail.com>
2015-12-03 12:03:03 -0500

    gray code (inspired

---
+++ leetcode/gray-code/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function (n) {
+  var results = [];
+  for (var i = 0; i < 1 << n; i++) {
+    results.push((i ^ i >> 1).toString(2));
+  }
+  return results;
+};
+
+console.log(grayCode(5));
