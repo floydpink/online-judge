de6c2fdcccb5a344142ffa7e7362be61b9989fb0
Floyd Pink <imbleedingme@googlemail.com>
2015-12-07 16:46:11 -0500

    merge intervals

--- leetcode/intersection-of-two-linked-lists/index.js
+++ leetcode/intersection-of-two-linked-lists/index.js
@@ -52,3 +52,21 @@ var getIntersectionNode = function (headA, headB) {
   }
   return null;
 };
+
+//var getIntersectionNode = function(headA, headB) {
+//    if (headA === null || headB === null) return null;
+//
+//    var n1 = headA, n2 = headB;
+//
+//    while (n1 !== null && n2 !== null && n1 !== n2) {
+//        n1 = n1.next;
+//        n2 = n2.next;
+//
+//        if (n1 === n2) return n1;
+//
+//        if (n1 === null) n1 = headB;
+//        if (n2 === null) n2 = headA;
+//    }
+//
+//    return n1;
+//};
---
+++ leetcode/merge-intervals/index.js
@@ -0,0 +1,32 @@
+//
+// https://leetcode.com/problems/merge-intervals/
+//
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
+var merge = function(intervals) {
+    if (intervals === null || intervals.length < 2) return intervals;
+    intervals = intervals.sort(function(a, b){ return a.start - b.start;});
+    var results = [], len = 0, current = intervals[0];
+    while (len < intervals.length - 1) {
+        var next = intervals[len + 1];
+        if (current.end >= next.start) {
+            current.end = Math.max(current.end, next.end);
+        } else {
+            results.push(current);
+            current = next;
+        }
+        len++;
+    }
+    results.push(current);
+    return results;
+};
