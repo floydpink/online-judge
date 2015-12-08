ff3ed76f2b0137d5ea9d78c72cd6b07f0ef345be
Floyd Pink <imbleedingme@googlemail.com>
2015-12-08 13:59:05 -0500

    merge sorted array

---
+++ leetcode/merge-sorted-array/index.js
@@ -0,0 +1,17 @@
+//
+// https://leetcode.com/problems/merge-sorted-array/
+//
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  var i = m - 1, j = n - 1, k = m + n - 1;
+  while (j >= 0) {
+    nums1[k--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+  }
+};
