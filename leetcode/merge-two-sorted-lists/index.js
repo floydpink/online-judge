621f614ec6e1d5ea6c73e3f6fb862cc173bda060
Floyd Pink <imbleedingme@googlemail.com>
2015-12-08 11:35:36 -0500

    merge two sorted lists

---
+++ leetcode/merge-two-sorted-lists/index.js
@@ -0,0 +1,36 @@
+//
+// https://leetcode.com/problems/merge-two-sorted-lists/
+//
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  var merged = new ListNode(0), current = merged;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+  if (l1 !== null) {
+    current.next = l1;
+  }
+  if (l2 !== null) {
+    current.next = l2;
+  }
+  return merged.next;
+};
