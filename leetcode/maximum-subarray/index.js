dc351bf1f91d53ca30d35aed864277f0137843c1
Floyd Pink <imbleedingme@googlemail.com>
2015-12-13 01:14:25 -0500

    maximum subarray

---
+++ leetcode/maximum-subarray/index.js
@@ -0,0 +1,19 @@
+//
+// https://leetcode.com/problems/maximum-subarray/
+//
+
+// https://leetcode.com/discuss/15805/accepted-o-n-solution-in-java
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    if (nums.length === 0) return 0;
+    var maxSoFar = nums[0], maxEndingHere = nums[0];
+    for (var i = 1; i< nums.length; i++) {
+        maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+};
+
